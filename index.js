f5d677b16182622ad99e622176d8c1c9ae319b11
David Konsumer <konsumer@jetboystudio.com>
2016-03-22 00:38:44 -0700

    moved data into widgets

--- index.js
+++ index.js
@@ -36,30 +36,12 @@ app.use(errorHandler)
 
 // TODO: actually run code
 
-// TODO: replace these with inline values in widgets, so they are more self-contained
-
-var blocks = []
-for (var i in mineflayer.blocks) {
-  blocks.push([mineflayer.blocks[i].displayName, mineflayer.blocks[i].id])
-}
-app.get('/blocks', function (req, res) {
-  res.send(blocks)
-})
-
 var items = []
 for (var i in mineflayer.items) {
   items.push([mineflayer.items[i].displayName, mineflayer.items[i].id])
 }
 app.get('/items', function (req, res) {
   res.send(items)
-})
-
-var instruments = []
-for (var i in mineflayer.instruments) {
-  instruments.push([mineflayer.instruments[i].name, mineflayer.instruments[i].id])
-}
-app.get('/instruments', function (req, res) {
-  res.send(instruments)
 })
 
 // emulate cloud-storage feature
