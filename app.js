e3a45b3b05f60d0862758fc14ff53884377602b5
David Konsumer <konsumer@jetboystudio.com>
2016-03-29 22:18:25 -0700

    basic code/open/save and start of run

--- app.js
+++ app.js
@@ -1,37 +1,79 @@
-var hl = require('highlight.js').highlight
-var beautify = require('js-beautify').js_beautify
+/* global Blockly */
 
-const remote = require('electron').remote;
-const Menu = remote.Menu;
-const MenuItem = remote.MenuItem;
+const electron = require('electron')
+const hl = require('highlight.js').highlight
+const beautify = require('js-beautify').js_beautify
+const dialog = electron.remote.require('dialog')
+const fs = require('fs')
+const SandCastle = require('sandcastle').SandCastle
+const sandcastle = new SandCastle({
+  timeout: 10000,
+  api: './run_api.js'
+})
 
+require('./blocks/common')
+require('./blocks/bot')
+require('./blocks/mod')
+require('./blocks/nonmc')
+
+const filters = [
+  { name: 'Xml', extensions: ['xml'] },
+  { name: 'All Files', extensions: ['*'] }
+]
+
+var codestyle = fs.readFileSync('node_modules/highlight.js/styles/github.css').toString()
+
+var blocklyDiv = document.getElementById('blocklyDiv')
+var blocklyArea = document.getElementById('blocklyArea')
 
 var template = [
   {
     label: 'File',
     submenu: [
       {
-        label: 'Run',
-        accelerator: 'CmdOrCtrl+R',
-        click: function(item, focusedWindow) {
+        label: 'Open',
+        accelerator: 'CmdOrCtrl+O',
+        click: function (item, focusedWindow) {
+          dialog.showOpenDialog({filters: filters}, function (filename) {
+            var xml = Blockly.Xml.textToDom(fs.readFileSync(filename[0]))
+            Blockly.Xml.domToWorkspace(workspace, xml)
+          })
         }
       },
       {
         label: 'Save',
         accelerator: 'CmdOrCtrl+S',
-        click: function(item, focusedWindow) {
+        click: function (item, focusedWindow) {
+          dialog.showSaveDialog({filters: filters}, function (filename) {
+            var xml = Blockly.Xml.workspaceToDom(workspace)
+            var xml_text = Blockly.Xml.domToText(xml)
+            fs.writeFileSync(filename, xml_text)
+          })
         }
       },
       {
-        label: 'Save As',
-        accelerator: 'Shift+CmdOrCtrl+S',
-        click: function(item, focusedWindow) {
-        }
-      },
-      {
-        label: 'Open',
-        accelerator: 'CmdOrCtrl+O',
-        click: function(item, focusedWindow) {
+        label: 'Run',
+        accelerator: 'CmdOrCtrl+R',
+        click: function (item, focusedWindow) {
+          const code = Blockly.JavaScript.workspaceToCode(window.workspace)
+          const script = sandcastle.createScript(code)
+
+          script.on('exit', (err, output) => {
+            if (err) {
+              return console.error(err)
+            }
+            console.log(output)
+          })
+
+          script.on('timeout', () => {
+            console.error('timed out.')
+          })
+
+          script.on('error', (err) => {
+            console.error(err)
+          })
+
+          script.run()
         }
       }
     ]
@@ -71,7 +113,7 @@ var template = [
         label: 'Select All',
         accelerator: 'CmdOrCtrl+A',
         role: 'selectall'
-      },
+      }
     ]
   },
   {
@@ -80,35 +122,42 @@ var template = [
       {
         label: 'Code',
         accelerator: 'Shift+CmdOrCtrl+C',
-        click: function(item, focusedWindow) {
+        click: function (item, focusedWindow) {
+          var code = Blockly.JavaScript.workspaceToCode(window.workspace)
+          const html = '<head><style>' + codestyle + '</style></head><body><pre><code>' + hl('javascript', beautify(code)).value + '</code></pre></body>'
+          window.open('data:text/html,' + encodeURIComponent(html))
         }
       },
       {
         label: 'Toggle Full Screen',
-        accelerator: (function() {
-          if (process.platform == 'darwin')
-            return 'Ctrl+Command+F';
-          else
-            return 'F11';
+        accelerator: (function () {
+          if (process.platform === 'darwin') {
+            return 'Ctrl+Command+F'
+          } else {
+            return 'F11'
+          }
         })(),
-        click: function(item, focusedWindow) {
-          if (focusedWindow)
-            focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+        click: function (item, focusedWindow) {
+          if (focusedWindow) {
+            focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
+          }
         }
       },
       {
         label: 'Toggle Developer Tools',
-        accelerator: (function() {
-          if (process.platform == 'darwin')
-            return 'Alt+Command+I';
-          else
-            return 'Ctrl+Shift+I';
+        accelerator: (function () {
+          if (process.platform === 'darwin') {
+            return 'Alt+Command+I'
+          } else {
+            return 'Ctrl+Shift+I'
+          }
         })(),
-        click: function(item, focusedWindow) {
-          if (focusedWindow)
-            focusedWindow.toggleDevTools();
+        click: function (item, focusedWindow) {
+          if (focusedWindow) {
+            focusedWindow.toggleDevTools()
+          }
         }
-      },
+      }
     ]
   },
   {
@@ -124,7 +173,7 @@ var template = [
         label: 'Close',
         accelerator: 'CmdOrCtrl+W',
         role: 'close'
-      },
+      }
     ]
   },
   {
@@ -133,14 +182,14 @@ var template = [
     submenu: [
       {
         label: 'Learn More',
-        click: function() { require('electron').shell.openExternal('https://github.com/konsumer/botmodblock') }
-      },
+        click: function () { electron.shell.openExternal('https://github.com/konsumer/botmodblock') }
+      }
     ]
-  },
-];
+  }
+]
 
-if (process.platform == 'darwin') {
-  var name = require('electron').remote.app.getName();
+if (process.platform === 'darwin') {
+  var name = electron.remote.app.getName()
   template.unshift({
     label: name,
     submenu: [
@@ -179,12 +228,12 @@ if (process.platform == 'darwin') {
       {
         label: 'Quit',
         accelerator: 'Command+Q',
-        click: function() { app.quit(); }
-      },
+        click: function () { window.close() }
+      }
     ]
-  });
+  })
   // Window menu.
-  template[3].submenu.push(
+  template[4].submenu.push(
     {
       type: 'separator'
     },
@@ -192,25 +241,18 @@ if (process.platform == 'darwin') {
       label: 'Bring All to Front',
       role: 'front'
     }
-  );
+  )
 }
 
-var menu = Menu.buildFromTemplate(template);
-Menu.setApplicationMenu(menu);
+var menu = electron.remote.Menu.buildFromTemplate(template)
+electron.remote.Menu.setApplicationMenu(menu)
 
-
-require('./blocks/common')
-require('./blocks/bot')
-require('./blocks/mod')
-require('./blocks/nonmc')
-
-
-window.workspace = window.Blockly.inject(blocklyDiv, {
+const workspace = window.Blockly.inject(blocklyDiv, {
   toolbox: document.getElementById('blocklyToolbox'),
   trashcan: true,
-  zoom:{
-    controls: true,
-    wheel: true,
+  zoom: {
+    controls: false,
+    wheel: false,
     startScale: 1.0,
     maxScale: 3,
     minScale: 0.3,
@@ -219,11 +261,8 @@ window.workspace = window.Blockly.inject(blocklyDiv, {
   media: 'blockly/media/'
 })
 
-
 // TODO: this sucks and only half-works.
 function onresize (e) {
-  var blocklyDiv = document.getElementById('blocklyDiv')
-  var blocklyArea = document.getElementById('blocklyArea')
   var element = blocklyArea
   var x = 0
   var y = 0
--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-'use strict';
+'use strict'
 
-const electron = require('electron');
+const electron = require('electron')
 // Module to control application life.
-const app = electron.app;
+const app = electron.app
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
+const BrowserWindow = electron.BrowserWindow
 
 const ipcMain = electron.ipcMain
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let mainWindow
+
+app.setName('botmodblock')
 
 function createWindow () {
   // Create the browser window.
@@ -18,25 +20,25 @@ function createWindow () {
     width: 800,
     height: 600,
     preload: __dirname + '/prompt.js'
-  });
+  })
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null;
-    app.quit();
-  });
+    mainWindow = null
+    app.quit()
+  })
 
   var promptResponse
-  ipcMain.on('prompt', function(eventRet, arg) {
+  ipcMain.on('prompt', function (eventRet, arg) {
     promptResponse = null
     var promptWindow = new BrowserWindow({
       width: 200,
@@ -55,17 +57,17 @@ function createWindow () {
     <style>body {font-family: sans-serif;} button {float:right; margin-left: 10px;} label,input {margin-bottom: 10px; width: 100%; display:block;}</style>'
     promptWindow.loadURL('data:text/html,' + promptHtml)
     promptWindow.show()
-    promptWindow.on('closed', function() {
+    promptWindow.on('closed', function () {
       eventRet.returnValue = promptResponse
       promptWindow = null
     })
   })
-  ipcMain.on('prompt-response', function(event, arg) {
-    if (arg === ''){ arg = null }
+  ipcMain.on('prompt-response', function (event, arg) {
+    if (arg === '') { arg = null }
     promptResponse = arg
   })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', createWindow);
+app.on('ready', createWindow)
